e3270c20de885d8e086b60ff6820cc7f6cc426b2
wurducius <wurducius@gmail.com>
2024-08-15 01:48:54 +0200

    Tweak docker test

--- docker-test/docker-test.sh
+++ docker-test/docker-test.sh
@@ -4,6 +4,7 @@ echo "Eofol3 Docker test"
 echo "[1/3] Cloning Eofol4..."
 git clone https://github.com/wurducius/eofol4
 cd eofol4
+rm ./package-lock.json
 echo "[2/3] Installing Eofol4..."
 npm i
 echo "[3/3] Running Eofo43..."
