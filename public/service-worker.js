845d5217de093738463ab588912067bca7ba6bb6
wurducius <wurducius@gmail.com>
2024-08-18 20:58:44 +0200

    Add service worker install pwa

---
+++ public/service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "cache"
+const CACHE_VERSION = "v1"
+
+// @TODO inject collected views
+const urlsToCache = ["index.html"]
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(`${CACHE_NAME}-${CACHE_VERSION}`).then((cache) => cache.addAll(urlsToCache)))
+})
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response
+      }
+      return fetch(event.request)
+    }),
+  )
+})
