36cc021f93ad8fe10e2c3d018d44cff2d34ecca8
wurducius <wurducius@gmail.com>
2024-08-19 01:31:13 +0200

    Try to fix docker testing loading bundle for generated pages

--- src/map.ts
+++ src/map.ts
@@ -4,14 +4,15 @@ import { injectElement, registerServiceworker } from "./util"
 // @TODO automate collect views
 const collectedViews = ["index.html", "index2.html", "license.html", "map.html", "404.html"]
 
+const capitalize = (str: string) =>
+  str
+    .split("")
+    .map((letter: string, i: number) => (i === 0 ? letter.toUpperCase() : letter))
+    .join("")
+
 const maplistContent = collectedViews
-  .map((view) =>
-    view
-      .split("")
-      .map((letter, i) => (i === 0 ? letter.toUpperCase() : letter))
-      .join(""),
-  )
-  .map((view) => `<li><a href="${view}">${view.replace(".html", "")}</a></li>`)
+  .map((view) => view)
+  .map((view) => `<li><a href="${view}">${capitalize(view).replace(".html", "")}</a></li>`)
   .join("")
 
 injectElement("map-list", maplistContent, Boolean(hexToCSSFilter))
