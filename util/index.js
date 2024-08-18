0072cb8e16afe19a9e159680e8632304fad4ccaa
wurducius <wurducius@gmail.com>
2024-08-18 21:35:05 +0200

    Add sleep function to util

--- util/index.js
+++ util/index.js
@@ -1,4 +1,9 @@
 const Fs = require("./fs")
 const DevUtil = require("./dev-util")
 
-module.exports = { ...Fs, ...DevUtil }
+const sleep = (ms) =>
+  new Promise((resolve) => {
+    setTimeout(resolve, ms)
+  })
+
+module.exports = { ...Fs, ...DevUtil, sleep }
