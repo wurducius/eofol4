0a40a3077322dabf47846b7757ebe3afc0f32d2e
wurducius <wurducius@gmail.com>
2024-08-19 18:28:36 +0200

    Setup image processing options

--- compiler/scripts/img.js
+++ compiler/scripts/img.js
@@ -3,9 +3,9 @@ const { optimize } = require("svgo")
 const { read } = require("../../util")
 const { jpegOptions, pngOptions, svgOptions } = require("../../plugin/options")
 
-const processJpeg = (filePath) => sharp(filePath).jpeg(jpegOptions).toBuffer()
+const processJpeg = (filePath) => sharp(filePath).jpeg(jpegOptions).withMetadata().toBuffer()
 
-const processPng = (filePath) => sharp(filePath).png(pngOptions).toBuffer()
+const processPng = (filePath) => sharp(filePath).png(pngOptions).withMetadata().toBuffer()
 
 const processSvg = (filePath) => optimize(read(filePath).toString(), svgOptions(filePath)).data
 
--- plugin/options.js
+++ plugin/options.js
@@ -1,6 +1,6 @@
-const jpegOptions = { quality: 25 }
+const jpegOptions = { quality: 25, mozjpeg: true, force: true }
 
-const pngOptions = { compressionLevel: 9, quality: 60, effort: 10 }
+const pngOptions = { compressionLevel: 9, quality: 60, effort: 10, adaptiveFiltering: true, force: true }
 
 const svgOptions = (filePath) => ({
   path: filePath,
