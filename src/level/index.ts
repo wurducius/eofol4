2c1435ea1b3619e0606d79b4d79139b1e3ee5a59
wurducius <wurducius@gmail.com>
2024-08-19 20:47:06 +0200

    Allow page tree structure

---
+++ src/level/index.ts
@@ -0,0 +1,7 @@
+import { hexToCSSFilter } from "hex-to-css-filter"
+import { injectElement, registerServiceworker } from "../util"
+
+injectElement("script", "Script injected and working!", true)
+injectElement("module", "External dependency imported and working!", Boolean(hexToCSSFilter))
+
+registerServiceworker("../")
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,17 +1,19 @@
 const EofolPlugin = require("../plugin")
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin
-const { resolve } = require("../util")
+const { resolve, parse } = require("../util")
 const { PATH_SRC, PATH_BUILD } = require("../config")
 const { exists } = require("../util/fs")
+const { sep } = require("path")
 
 const getWebpackConfig = (params) => {
   const { views, mode, analyze } = params
 
   // @TODO handle also .js scripts
   const entry = views.reduce((acc, next) => {
-    const viewPath = resolve(PATH_SRC, `${next}.ts`)
+    const parsed = parse(next)
+    const viewPath = resolve(PATH_SRC, parsed.dir, `${parsed.name}.ts`)
     if (exists(viewPath)) {
-      return { ...acc, [next]: viewPath }
+      return { ...acc, [[parsed.dir.replace(sep, "/"), parsed.name].filter(Boolean).join("/")]: viewPath }
     } else {
       return acc
     }
