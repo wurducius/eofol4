45a3fa1ca37aa335810ad70a0b342fc62decfe89
wurducius <wurducius@gmail.com>
2024-09-01 23:26:12 +0200

    Extract prune and updateDom

---
+++ runtime/stateful/prune.ts
@@ -0,0 +1,13 @@
+import { onComponentUnmount, onComponentUnmounted } from "./lifecycle"
+import { getInstances } from "../internals"
+
+export const prune = () => {
+  // @TODO FIXME UNMOUNT
+  Object.keys(getInstances()).forEach((id) => {
+    const element = document.getElementById(id)
+    if (!element) {
+      onComponentUnmount(id)
+      onComponentUnmounted(id)
+    }
+  })
+}
