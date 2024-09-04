6225cf7f68d607cafdb70bca60826e731fbac134
wurducius <wurducius@gmail.com>
2024-09-04 02:33:30 +0200

    Copy util helpers

---
+++ runtime/geolocation/index.ts
@@ -0,0 +1,13 @@
+export const getGeolocation = (options): Promise<GeolocationPosition> => {
+  return new Promise((resolve, reject) =>
+    navigator.geolocation.getCurrentPosition(
+      resolve,
+      reject,
+      options ?? {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+      },
+    ),
+  )
+}
